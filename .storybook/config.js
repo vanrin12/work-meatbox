c2c41d20193efc8f99408b1f2711dcfa0018d0ee
rinto0503 <rinto0503@gmail.com>
2019-11-17 20:35:39 +0700

    Update config storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,7 +4,7 @@ import { configure, addDecorator } from '@storybook/react';
 import '../src/App.scss';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context('../src', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
